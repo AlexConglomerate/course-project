a405a9b514d093f830bc6c16edc4ad3def4840f1
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-09-14 14:01:55 +0500

    Add deploy in package.json

---
+++ build/precache-manifest.000789b70972d19b34da7f44e45ce224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44f93c1f60d047456ae2",
+    "url": "/course-project/static/js/main.44f93c1f.chunk.js"
+  },
+  {
+    "revision": "9c90ba28cf8648290dde",
+    "url": "/course-project/static/css/1.2da20fcf.chunk.css"
+  },
+  {
+    "revision": "9c90ba28cf8648290dde",
+    "url": "/course-project/static/js/1.9c90ba28.chunk.js"
+  },
+  {
+    "revision": "127f4f50f858e84d05b7",
+    "url": "/course-project/static/js/runtime~main.127f4f50.js"
+  },
+  {
+    "revision": "f75e5c7c933150b71fbe99d99f182614",
+    "url": "/course-project/index.html"
+  }
+];
