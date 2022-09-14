afb91ae162864b11136451432d086bf59b042643
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-09-14 16:59:13 +0500

    Сделал композицию

--- src/Table.js
+++ src/Table.js
@@ -1,78 +1,61 @@
 import React, {useState} from "react";
+// {useState} - это добавление хука useState
 import {fetchAll} from "./API/fake.api/user.api";
 
 const Table = () => {
 
-    const [users, setUsers] = useState(fetchAll())
+    const [users, setUsers] = useState(0)
+    const [dog, setDogs] = useState(["dog1", "dog2", "dog3"])
 
-    const deleteUser = (id) => {
-        setUsers(prevState => prevState.filter(a => a._id != id))
+
+    const btnClick = () => {
+        setUsers(users + 1)
+
+        // Если нужно два раза:
+
+        // // не работает
+        // setUsers(users + 1)
+        // setUsers(users + 1)
+
+        // // Работает. prevState - текущее значение
+        // setUsers(prevState => prevState + 1)
+        // setUsers(prevState => prevState + 1)
+        console.log(users)
     }
 
     const createHeader = () => {
-        let count = users.length
+        let text = `Надпись`
+        let cls = "h3 fw-bold text-white m-1 p-1 rounded-pill bg-primary"
 
-        // create text
-        let addText = ""
-        if (count > 1 && count < 5 || count > 21) addText = "a"
-        let text = `${count} человек${addText} тусанет с тобой сегодня`
-        if (count == 0) text = `Никто с тобой не тусанет`
+        let btn = <div
+            className="btn btn-danger"
+            onClick={() => btnClick()}
+        > My button</div>
 
-        //create color
-        let cls = "h3 fw-bold text-white m-1 p-1 rounded-pill bg-"
-        count == 0 ? cls += "danger" : cls += "primary"
-
-        return <span className={cls}> {text} </span>
-
+        return (
+            <> {/* <> - не будет отображаться в HTML. () - чтобы return понимал границы return*/}
+                <span className={cls}> {text} </span>
+                <span>{btn}</span>
+            </>)
     }
 
-    const createTable = () => {
-        if (users.length == 0) return <img
-            src="https://www.meme-arsenal.com/memes/9a03f2e29dcaab7c64defc4afb265a5c.jpg"/>
-
-        return <table className="table table-hover">
-            <thead>
-            <tr>
-                <td>Имя</td>
-                <td>Качества</td>
-                <td>Профессия</td>
-                <td>Встретился, раз</td>
-                <td>Оценка</td>
-                <td>Button</td>
-            </tr>
-            </thead>
-
-            <tbody>
-            {users.map(item => (
-                <tr>
-                    <td>{item.name}</td>
-                    <td>{item.qualities.map(a => {
-                        let clr = "badge m-1 p-1 bg-" + a.color
-                        return <span className={clr}>
-                            {a.name}
-                        </span>
-                    })}</td>
-                    <td>{item.profession.name}</td>
-                    <td>{item.completedMeetings}</td>
-                    <td>{item.rate + "/5"}</td>
-                    <td>{
-                        <div
-                            className="btn btn-danger"
-                            onClick={() => deleteUser(item._id)}
-                        >
-                            Delete</div>}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+    const createDogList = () => {
+        dog.map(i => console.log(`123`))
+        dog.map(i => (
+            <li
+                // key={i}
+                // onClick={btnClick}
+            >
+                {i}
+            </li>
+        ))
     }
 
     return (
 
         <>
             {createHeader()}
-            <br/>HiHiHiHiHiHi<br/>
-            {createTable()}
+            {createDogList()}
         </>
     )
 }
