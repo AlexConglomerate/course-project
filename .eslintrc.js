fbdc090f867b1e7aed474f46ee416975e6f078ca
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-11-16 20:33:49 +0600

    Развернул на сайте

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        indent: ["error", 4], // Отступ количество пробелов
+        semi: [2, "always"], // Точка с запятой в конце строки
+
+        // Ошибка при наличии пробела при обозначении функции, уберём её
+        "space-before-function-paren": ["error", {anonymous: "always", named: "never"}],
+
+        // Использование двойных кавычек
+        quotes: ["error", "double", {allowTemplateLiterals: true}]
+    },
+};
