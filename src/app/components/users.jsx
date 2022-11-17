776efc4e5875e7f91ba7a92b372622bdc7c00025
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-11-17 13:11:36 +0600

    6. Сортировка | Ячейка вложенного компонента

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -40,7 +40,7 @@ const Users = ({users, ...rest}) => {
         ? users.filter(user => JSON.stringify(user.profession) === JSON.stringify(selectedProf))
         : users
     const count = filteredUsers.length
-    const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order])
+    const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order])
     const userCrop = paginate(sortedUsers, currentPage, pageSize)
     const clearFilter = () => {
         setSelectedProf()
--- src/app/components/usersTable.jsx
+++ src/app/components/usersTable.jsx
@@ -3,17 +3,45 @@ import React from 'react';
 import PropTypes from "prop-types";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
+import Bookmark from "./bookmark";
+import BookMark from "./bookmark";
 
 
-function UsersTable({users, onSort, selectedSort, ...rest}) {
+function UsersTable({
+                        users,
+                        onSort,
+                        selectedSort,
+                        onToggleBookmark,
+                        onDelete,
+                        ...rest
+                    }) {
     const columns = {
         name: {path: 'name', name: 'Имя'},
         qualities: {name: 'Качества'},
         professions: {path: 'profession.name', name: 'Профессия'},
         completedMeetings: {path: 'completedMeetings', name: 'Встретился, раз'},
         rate: {path: 'rate', name: 'Оценка'},
-        bookmark: {path: 'bookmark', name: 'Избранное'},
-        delete: {},
+        bookmark: {
+            path: 'bookmark', name: 'Избранное',
+            component: (user) => (
+                <BookMark
+                    status={user.bookmark}
+                    user={user}
+                    onToggleBookmark={user._id}
+                    {...rest}
+                />
+            )
+        },
+        delete: {
+            component: (user) => (
+                <button
+                    onClick={() => onDelete(user._id)}
+                    className="btn btn-danger"
+                >
+                    delete
+                </button>
+            )
+        },
     }
 
     return (
@@ -28,6 +56,8 @@ UsersTable.propTypes = {
     users: PropTypes.array.isRequired,
     onSort: PropTypes.func.isRequired,
     selectedSort: PropTypes.object.isRequired,
+    onToggleBookmark: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
 }
 
 export default UsersTable;
