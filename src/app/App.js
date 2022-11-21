472758d76a0c3f86216a4e9ec7959fe052127055
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-11-21 11:13:00 +0600

    9. Формы. Часть 1 | Перед ДЗ

--- src/app/App.js
+++ src/app/App.js
@@ -1,32 +1,22 @@
 import React from "react";
-import NavBar from "./components/navBar";
-import {Redirect, Route, Switch} from "react-router-dom";
-import Users from "./components/users";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
-import TestPage from "./layouts/testPage";
-import User from "./components/user";
+import NavBar from "./components/navBar";
 
 function App() {
     return (
-        <>
-            <NavBar/> {/* списки URL*/}
+        <div>
+            <NavBar />
             <Switch>
-                {/* exact - полное совпадение*/}
-                <Route path="/" exact component={Main}/>
-                {/* if (url == "/login") => вывести компонент Login */}
-                <Route path="/login" component={Login}/>
-                <Route path="/users/:userId" component={User}/>
-                <Route path="/users" component={Users}/>
-                <Route path="/404" render={() => (<h1>Page not found</h1>)}/>
-                <Redirect from="/people" to="/users"/>
-                <Redirect to="/404"/>
-
-                {/*если хотим передать ещё какие-то парраметры, кроме props*/}
-                <Route path="/testPage" render={(props) => (<TestPage posts={[1, 2, 3]} {...props}/>)}/>
+                <Route path="/users/:userId?" component={Users} />
+                <Route path="/login" component={Login} />
+                <Route path="/" exact component={Main} />
+                <Redirect to="/" />
             </Switch>
-            {/*<Users/>*/}
-        </>
+        </div>
     );
 }
 
--- src/app/components/navBar.jsx
+++ src/app/components/navBar.jsx
@@ -1,41 +1,25 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
-        <div className="d-flex flex-row">
-            <Link className="p-1" to='/'>Main</Link>
-            <Link className="p-1" to='/login'>Login</Link>
-            <Link className="p-1" to='/users'>Users</Link>
-        </div>
-
-        // <ul>
-        //     <li>
-        //         <Link to='/'>Main</a>
-        //     </li>
-        //     <li>
-        //         <Link to='/login'>Login</a>
-        //     </li>
-        //     <li>
-        //         <Link to='/users'>Users</a>
-        //     </li>
-        // </ul>
-        // <ul>
-        //     <li>
-        //         <Link to="/">Home</Link>
-        //     </li>
-        //     <li>
-        //         <Link to="/login">Login</Link>
-        //     </li>
-        //     <li>
-        //         <Link to="/posts">Posts</Link>
-        //     </li>
-        //     <li>
-        //         <Link to="/dashboard">Dashboard</Link>
-        //     </li>
-        // </ul>
-    )
-        ;
+        <ul className="nav">
+            <li className="nav-item">
+                <Link className="nav-link " aria-current="page" to="/">
+                    Main
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link " aria-current="page" to="/login">
+                    Login
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link " aria-current="page" to="/users">
+                    Users
+                </Link>
+            </li>
+        </ul>
+    );
 };
 
 export default NavBar;
