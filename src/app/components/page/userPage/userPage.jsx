280184a67fa81454ec067da764bf4ca35cf4541b
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-12-08 11:42:42 +0600

    Вынес userCard.jsx

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,10 +1,10 @@
 import React, {useEffect, useRef, useState} from "react";
 import api from "../../../api";
-import Qualities from "../../ui/qualities";
 import {useHistory} from "react-router-dom";
 import {useParams} from "react-router-dom";
 import Comments from "./comments";
 import {orderBy} from "lodash";
+import UserCard from "./userCard";
 
 
 function UserPage({userId}) {
@@ -16,7 +16,6 @@ function UserPage({userId}) {
     const [user, setUser] = useState()
     const [comment, setComment] = useState()
     const [users, setUsers] = useState()
-    const [changeUser, setChangeUser] = useState()
 
     const updateComment = () => {
         api.comments.fetchCommentsForUser(params.userId).then((data) => {
@@ -30,11 +29,6 @@ function UserPage({userId}) {
         updateComment()
     }, []);
 
-
-    const handleChangeUser = (user) => {
-        setChangeUser(user)
-        console.log(changeUser)
-    }
 
     const handleClick = () => history.push(`/users/${user._id}/edit`)
 
@@ -61,59 +55,7 @@ function UserPage({userId}) {
         <div className="container">
             <div className="row gutters-sm">
                 <div className="col-md-4 mb-3">
-                    <div className="card mb-3">
-                        <div className="card-body">
-                            <button className="position-absolute top-0 end-0 btn btn-light btn-sm">
-                                <i className="bi bi-gear" onClick={handleClick}></i>
-                            </button>
-                            <div className="d-flex flex-column align-items-center text-center position-relative">
-                                <img src={`https://avatars.dicebear.com/api/avataaars/${(Math.random() + 1)
-                                    .toString(36)
-                                    .substring(7)}.svg`}
-                                     className="rounded-circle shadow-1-strong me-3"
-                                     alt="avatar"
-                                     width="150"
-                                />
-
-                                <div className="mt-3">
-                                    <h4>{user.name}</h4>
-                                    <p className="text-secondary mb-1">{user.profession.name}</p>
-                                    <div className="text-muted">
-                                        <i className="bi bi-caret-down-fill text-primary                                            "
-                                           role="button"
-                                        ></i>
-                                        <i className="bi bi-caret-up text-secondary"
-                                           role="button"
-                                        ></i>
-                                        <span className="ms-2">{user.rate}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card mb-3">
-                        <div className="card-body d-flex flex-column justify-content-center text-center">
-                            <h5 className="card-title">
-                                <span>Qualities</span>
-                            </h5>
-                            <p className="card-text">
-                                <Qualities qualities={user.qualities}/>
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="card mb-3">
-                        <div className="card mb-3">
-                            <div className="card-body d-flex flex-column justify-content-center text-center">
-                                <h5 className="card-title">
-                                    <span>Completed meetings</span>
-                                </h5>
-
-                                <h1 className="display-1">{user.completedMeetings}</h1>
-                            </div>
-                        </div>
-                    </div>
+                    <UserCard user={user} handleClick={handleClick}/>
                 </div>
 
                 <div className="col-md-8">
@@ -121,17 +63,11 @@ function UserPage({userId}) {
                         <div className="card-body">
                             <div>
                                 <h2>New comment</h2>
-
                                 <select className="form-select mb-4" name="userId" ref={selectName}>
-                                    <option
-                                        selected
-                                    >Выберите пользователя
-                                    </option>
+                                    <option selected>Выберите пользователя</option>
                                     {users &&
                                         users.map(item => (
-                                            <option
-                                                key={item._id}
-                                            >
+                                            <option key={item._id}>
                                                 {item.name}
                                             </option>
                                         ))
