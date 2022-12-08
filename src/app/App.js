7ee0dbc56b80f67e388b3b422341c1719f6a350f
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-12-08 15:13:05 +0600

    Правильный вариант

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,20 @@
 import React from "react";
-import {Route, Switch, Redirect} from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
-import editUserPage from "./components/page/editUserPage";
 
 function App() {
     return (
         <div>
-            <NavBar/>
+            <NavBar />
             <Switch>
-                <Route path="/users/:userId?/edit" component={editUserPage}/>
-                <Route path="/users/:userId?" component={Users}/>
-                <Route path="/login/:type?" component={Login}/>
-                <Route path="/" exact component={Main}/>
-                <Redirect to="/"/>
+                <Route path="/users/:userId?/:edit?" component={Users} />
+                <Route path="/login/:type?" component={Login} />
+                <Route path="/" exact component={Main} />
+                <Redirect to="/" />
             </Switch>
         </div>
     );
--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,91 +1,38 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import api from "../../../api";
-import {useHistory} from "react-router-dom";
-import {useParams} from "react-router-dom";
-import Comments from "./comments";
-import {orderBy} from "lodash";
-import UserCard from "./userCard";
-import AddComment from "./addComment";
-import CommentList from "./commentList";
+import UserCard from "../../ui/userCard";
+import QualitiesCard from "../../ui/qualitiesCard";
+import MeetingsCard from "../../ui/meetingsCard";
+import Comments from "../../ui/comments";
 
-
-function UserPage({userId}) {
-    const textComment = useRef()
-    const selectName = useRef()
-    const params = useParams()
-    const history = useHistory()
-
-    const [user, setUser] = useState()
-    const [comment, setComment] = useState()
-    const [users, setUsers] = useState()
-
-    const updateComment = () => {
-        api.comments.fetchCommentsForUser(params.userId).then((data) => {
-            setComment(orderBy(data, ['created_at'], ['desc']))
-        })
-        console.log(comment)
-    }
-
+const UserPage = ({ userId }) => {
+    const [user, setUser] = useState();
     useEffect(() => {
-        api.users.getById(userId).then((data) => setUser(data))
-        api.users.fetchAll().then((data) => setUsers(data))
-        updateComment()
+        api.users.getById(userId).then((data) => setUser(data));
     }, []);
-
-    const handleClick = () => history.push(`/users/${user._id}/edit`)
-
-    const handleRefresh = (text, selectName) => {
-        if (selectName.current.value === 'Выберите пользователя') {
-            window.alert('Выберите пользователя')
-            return
-        }
-        if (text.current.value === '') {
-            window.alert('Напишите комментарий')
-            return
-        }
-        api.comments.add({
-            userId: users.find(item => item.name == selectName.current.value)._id,
-            pageId: userId,
-            content: text.current.value,
-        },).then()
-        text.current.value = ''
-        selectName.current.value = ''
-        updateComment()
-    }
-
-    const handleRemoveComment = (id) => {
-        api.comments.remove(id).then();
-        updateComment()
-    }
-
-    if (!user || !comment) return <h1>Loading</h1>
-
-    return (
-        <div className="container">
-            <div className="row gutters-sm">
-                <div className="col-md-4 mb-3">
-                    <UserCard
-                        user={user}
-                        handleClick={handleClick}
-                    />
-                </div>
-
-                <div className="col-md-8">
-                    <AddComment
-                        handleRefresh={handleRefresh}
-                        users={users}
-                        textComment={textComment}
-                        selectName={selectName}
-                    />
-
-                    <CommentList
-                        comment={comment}
-                        handleRemoveComment={handleRemoveComment}
-                    />
+    if (user) {
+        return (
+            <div className="container">
+                <div className="row gutters-sm">
+                    <div className="col-md-4 mb-3">
+                        <UserCard user={user} />
+                        <QualitiesCard data={user.qualities} />
+                        <MeetingsCard value={user.completedMeetings} />
+                    </div>
+                    <div className="col-md-8">
+                        <Comments />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        );
+    } else {
+        return <h1>Loading</h1>;
+    }
+};
+
+UserPage.propTypes = {
+    userId: PropTypes.string.isRequired
+};
 
 export default UserPage;
